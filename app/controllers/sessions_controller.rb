ec80927c2c4c914436a660212891681e62b884b0
kewoodh <kesean.woodhouse@gmail.com>
2017-08-18 09:56:48 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  session[:name] = params[:name]
+    if session[:name] == nil || session[:name].empty?
+      redirect_to(controller: 'sessions', action: 'new')
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to '/'
+  end
+
+end
